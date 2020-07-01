eceea62230cb210f9592a233fca24b9cd6f20d9e
yangpan12 <738912168@qq.com>
2020-07-01 11:27:33 +0800

    fix:测试聊天器

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    测试
+    <uncle-vue-chat :feedList="feedList" :author="author"/>
   </div>
 </template>
 
 
 <script>
+import uncleVueChat from 'uncle-vue-chat';
+
 export default {
   name: 'test',
   data() {
     return {
+      feedList: [
+        {
+          key: 0,
+          id: 0,
+          author: 'Person',
+          contents: 'hi there',
+          date: '16:30',
+          type: 'message',
+        },
+      ],
+      author: 1,
     };
   },
   components: {
+    uncleVueChat,
   },
   mounted() {
 
